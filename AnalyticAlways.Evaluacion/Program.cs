4975245581222f4d5a171f0b015463fec5594c4d
Montano <manuelelp@gmail.com>
2020-03-13 14:01:18 +0100

    Primer commit

---
+++ AnalyticAlways.Evaluacion/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace AnalyticAlways.Evaluacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("app.json");
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+        }
+    }
+}
